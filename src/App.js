7616af80204b622caa7d1e752d360e98a4c9f8ba
David <david.geretti@gmail.com>
2025-03-21 15:51:34 +0100

    Setup app layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
 import React from 'react'
-import { Container } from '@mantine/core'
-import Header from './components/Header'
+import { AppShell, Burger, Group, NavLink, Title } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './Routes'
 
+import SetupScreen from './screens/SetupScreen'
+import RecommendedScreen from './screens/RecommendedScreen'
+import CurrentScreen from './screens/CurrentScreen'
+import ShoppingListScreen from './screens/ShoppingListScreen'
+
+import { Gear, Star, List, ShoppingCart } from '@phosphor-icons/react'
+
+const routes = [
+  { label: 'Setup', icon: Gear, link: '/setup', component: SetupScreen },
+  { label: 'Recommended', icon: Star, link: '/recommended', component: RecommendedScreen },
+  { label: 'Current', icon: List, link: '/current', component: CurrentScreen },
+  { label: 'Shopping list', icon: ShoppingCart, link: '/shopping-list', component: ShoppingListScreen }
+];
+
 function App() {
+
+  const [opened, { toggle }] = useDisclosure();
+
   return (
     <BrowserRouter>
-      <Container size="md" py="xl">
-        <Header />
-        <AppRoutes />
-      </Container>
+      <AppShell
+        header={{ height: 100 }}
+        navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+        padding="md"
+      >
+        <AppShell.Header>
+          <Group h="100%" px="md">
+            <Title order={2}>Manage my stock</Title>
+            
+            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          </Group>
+        </AppShell.Header>
+        <AppShell.Navbar p="md">
+          {
+            routes.map((route) => (
+              <NavLink
+                href={route.link}
+                key={route.label}
+                label={route.label}
+                leftSection={<route.icon size={16} stroke={1.5} />}
+                active={window.location.pathname === route.link}
+              />
+            ))
+          }
+        </AppShell.Navbar>
+        <AppShell.Main>
+          <AppRoutes routes={routes} />
+        </AppShell.Main>
+      </AppShell>
     </BrowserRouter>
   )
 }
--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,13 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
-import SetupScreen from './screens/SetupScreen'
-import RecommendedScreen from './screens/RecommendedScreen'
-import CurrentScreen from './screens/CurrentScreen'
-import ShoppingListScreen from './screens/ShoppingListScreen'
 
-function AppRoutes() {
+function AppRoutes({ routes }) {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/setup" />} />
-      <Route path="/setup" element={<SetupScreen />} />
-      <Route path="/recommended" element={<RecommendedScreen />} />
-      <Route path="/current" element={<CurrentScreen />} />
-      <Route path="/shopping-list" element={<ShoppingListScreen />} />
+      <Route path="/" element={<Navigate to={routes[0].link} />} />
+      {routes.map((route) => (
+        <Route key={route.label} path={route.link} element={<route.component />} />
+      ))}
     </Routes>
   )
 }
