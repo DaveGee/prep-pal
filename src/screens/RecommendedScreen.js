4ca5b3f6ec27395f84f9397caa9b13dc3d38ce50
David <david.geretti@gmail.com>
2025-03-23 12:34:44 +0100

    Add the override quantity field in the table

--- src/screens/RecommendedScreen.js
+++ src/screens/RecommendedScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import { Container, Table, Title } from '@mantine/core'
+import { Container, NumberInput, Table, Title } from '@mantine/core'
 import recommendations from '../data/productCategories.json'
+import classes from './RecommendedScreen.module.css'
 
 function RecommendedScreen() {
   const [data, setData] = useState([])
@@ -9,25 +10,34 @@ function RecommendedScreen() {
     setData(recommendations.baseCategories)
   }, [])
 
-  const rows = data.map((item) => (
+  const rows = data.map((item, index) => (
     <Table.Tr key={item.productType}>
       <Table.Td>{item.productType}</Table.Td>
       <Table.Td>{item.description}</Table.Td>
       <Table.Td>{item.quantity}</Table.Td>
-      <Table.Td></Table.Td>
+      <Table.Td className={classes.overrideColumn}>
+        <NumberInput
+          value={item.quantityOverride || ''}
+          onChange={(value) => {
+            const updatedData = [...data]
+            updatedData[index] = { ...item, quantityOverride: value }
+            setData(updatedData)
+          }}
+        />
+      </Table.Td>
     </Table.Tr>
   ))
 
   return (
     <Container fluid>
       <Title order="1">Recommended product to have stored</Title>
-      <Table striped>
+      <Table striped highlightOnHover>
         <Table.Thead>
           <Table.Tr>
             <Table.Th>Product type</Table.Th>
             <Table.Th>Description</Table.Th>
             <Table.Th>Quantity</Table.Th>
-            <Table.Th>Override</Table.Th>
+            <Table.Th className={classes.overrideColumn}>Override</Table.Th>
           </Table.Tr>
         </Table.Thead>
         <Table.Tbody>{rows}</Table.Tbody>
